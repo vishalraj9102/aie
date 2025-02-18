e2bdbd0c59e2209c0407896640c38bbf74b95a09
vishalraj9102 <vishalrajmehra95@gmail.com>
2025-02-19 00:40:41 +0530

    first commit

--- django_auth_project/django_auth_project/settings.py
+++ django_auth_project/django_auth_project/settings.py
@@ -102,8 +102,8 @@ CSRF_COOKIE_HTTPONLY = False  # Allow JavaScript to access the CSRF cookie
 CORS_ALLOW_ALL_ORIGINS = True  # Or restrict to specific origins
 # CSRF and CORS Settings
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost']
-CORS_ALLOW_ALL_ORIGINS = True  # For testing; restrict in production
+CORS_ALLOW_ALL_ORIGINS = True  
 
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",  # Adjust your frontend URL here
+    "http://127.0.0.1:8000",  
 ]
